2ab8cd7f20a0628dcaf0b44e64421b54c56be78a
weijiangyang <weijiangyang@laposte.net>
2021-08-23 21:21:35 +0200

    --- Auto-Git Commit ---

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -22,7 +22,7 @@ exports.signup = (req, res, next) => {
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
-  };
+      };
 // login d'un user existant
   exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
