99b0ebecdfdafdac94fc7465ed25612cc794c025
weijiangyang <weijiangyang@laposte.net>
2021-08-23 15:31:11 +0200

    --- Auto-Git Commit ---

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,9 +1,13 @@
+//importer le modèle Sauce
 const Sauce = require('../models/sauce');
+
+// importer le package 'fs' 
 const fs = require("fs");
 
 
-
+// créer une nouvelle sauce
 exports.createSauce = (req, res, next) => {
+    //créer une instance de Sauce 
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
     const sauce = new Sauce({
@@ -11,29 +15,29 @@ exports.createSauce = (req, res, next) => {
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
       likes:0,
       dislikes:0
-
     });
+    // enregistrer le nouveau objet dans la base de données et renvoyer la reponse sur le status de la réponse et des messages de reusis ou échec  
     sauce.save()
       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
       .catch(error => res.status(400).json({ error }));
-  };
-
+    };
+// lire tous les sauces
 exports.getAllSauces = (req,res,next) => {
     Sauce.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(404).json({error}));
 };
 
-
+// lire une sauce spécifique
 exports.getOneSauce = (req,res,next) => {
     Sauce.findOne({_id:req.params.id })
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(400).json({error}));
 };
-
+// modifier une sauce
 exports.modifySauce = (req, res, next) => {
     
-    
+        //2 conditions pour la modification : il faut changer l'image ou non. 
         const sauceObject = req.file ?
         {
             ...JSON.parse(req.body.sauce),
@@ -43,11 +47,12 @@ exports.modifySauce = (req, res, next) => {
         .then(() => res.status(200).json({ message: 'Objet modifié !'}))
         .catch(error => res.status(400).json({ error }));
         }
-    
+// supprimer une sauce   
 exports.deleteSauce = (req,res,next) =>{
     Sauce.findOne({_id:req.params.id})
     .then(sauce => {
       const filename = sauce.imageUrl.split('/images/')[1];
+      // supprimer son fichier de l'image dans le serveur pendant la suppression de la sauce 
       fs.unlink(`images/${filename}`,() => {
         Sauce.deleteOne({_id:req.params.id })
         .then(() => res.status(200).json({message:'Objet supprime!'}))
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,10 +1,14 @@
+//importer le framework ' Express ' 
 const express = require('express');
 const router = express.Router();
+// importer le fichier controllers 
 const sauceCtrl = require('../controllers/sauce');
+// importer le fichier middleware pour l'authentification 
 const auth = require('../middleware/auth');
+// importer le fichier middleware pour le multer 
 const multer = require('../middleware/multer-config');
 
-
+// les routes pour chaque fonctionnement
 router.post('/',auth,multer, sauceCtrl.createSauce );
 
 router.get('/',auth, sauceCtrl.getAllSauces);
@@ -16,5 +20,7 @@ router.put('/:id',auth,multer,sauceCtrl.modifySauce);
 router.delete('/:id',auth,sauceCtrl.deleteSauce);
 
 router.post('/:id/like',auth,sauceCtrl.likeSauce);
+
+// exporter le modèle router pour les sauces
 module.exports = router;
 
