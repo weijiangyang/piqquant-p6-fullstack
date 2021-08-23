e98374e650f534d21e5897d29f8e3942d06afdfa
weijiangyang <weijiangyang@laposte.net>
2021-08-23 12:06:00 +0200

    --- Auto-Git Commit ---

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
-// const sauce = require('../controllers/sauce');
+
 
 router.post('/',auth,multer, sauceCtrl.createSauce );
 
