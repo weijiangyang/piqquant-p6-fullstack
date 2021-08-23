1cc0f4c49464524f64486aca24d8aa9d3d17e971
weijiangyang <weijiangyang@laposte.net>
2021-08-23 15:31:11 +0200

    --- Auto-Git Commit ---

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
+// importer le framework 'express'
 const express = require('express');
 const router = express.Router();
-
+// importer le fichier controller pour l'user
 const userCtrl = require('../controllers/user');
 
+// les routers pour le signup et le login de l'user
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
+// exporter le modèle de router pour l'user 
 module.exports = router;
