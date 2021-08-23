8200f18c3e976dd0fc1fad7a6e029c490dfba240
weijiangyang <weijiangyang@laposte.net>
2021-08-23 14:30:31 +0200

    --- Auto-Git Commit ---

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,10 +3,13 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
+
+
 exports.signup = (req, res, next) => {
    
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
+        
         const user = new User({
           email: req.body.email,
           password: hash
