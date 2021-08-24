7e1883dd35307cc74d0eafc1b197a06e4ba87253
weijiangyang <weijiangyang@laposte.net>
2021-08-24 20:54:42 +0200

    --- Auto-Git Commit ---

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,6 +10,20 @@ dotenv.config();
 
 // sign up d'un nouveau user
 exports.signup = (req, res, next) => {
+  const passwordValidator = require('password-validator');
+  var schema = new passwordValidator();
+  schema
+  .is().min(8)                                    // Minimum length 8
+  .is().max(100)                                  // Maximum length 100
+  .has().uppercase()                              // Must have uppercase letters
+  .has().lowercase()                              // Must have lowercase letters
+  .has().digits(2)                                // Must have at least 2 digits
+  .has().not().spaces()                           // Should not have spaces
+  .is().not().oneOf(['Passw0rd', 'Password123']); 
+  console.log(schema.validate('123'));
+  if (!schema.validate(req.body.password)){
+    window.alert("password incorrect")
+  }else{
    // hasher le mot de passe
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
@@ -25,6 +39,7 @@ exports.signup = (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
       };
+    };
 // login d'un user existant
   exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
