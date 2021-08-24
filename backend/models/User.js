c5a0ab1a018c337a0fd5a1bcb8071aa3c9589c67
weijiangyang <weijiangyang@laposte.net>
2021-08-24 20:24:22 +0200

    --- Auto-Git Commit ---

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //importer le plugin de mongoose 'mongoose-unique-validator'
 const uniqueValidator = require('mongoose-unique-validator');
 
+
 // configurer le userSchema
 const userSchema = mongoose.Schema({
   email: {type: String, required: true, unique: true },
@@ -11,3 +12,14 @@ const userSchema = mongoose.Schema({
 userSchema.plugin(uniqueValidator);
 // exporter le modèle userSchema sous le nom 'User'
 module.exports = mongoose.model('User', userSchema);
+const passwordValidator = require('password-validator');
+var schema = new passwordValidator();
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); 
+console.log(schema.validate('123'));
