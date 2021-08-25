d08825272ef10d57b93dfb2b4f56de1b20fb82b5
weijiangyang <weijiangyang@laposte.net>
2021-08-25 09:51:59 +0200

    --- Auto-Git Commit ---

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const rateLimit = require("express-rate-limit");
 const createAccountLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour window
+    windowMs: 2 * 60 * 1000, // 1 hour window
     max: 5, // start blocking after 5 requests
     message:
       "Too many accounts created from this IP, please try again after an hour"
